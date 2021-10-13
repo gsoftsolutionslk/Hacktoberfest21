6f32f0c9efc8b53e36f03c7deefb5c2419ab6d62
sanc <hlsandunichamika@gmail.com>
2021-10-13 20:34:20 +0530

    Created OOP Concepts and added inheritance.

---
+++ Java/OOPConcepts/Inheritance/MountainBike.java
@@ -0,0 +1,35 @@
+package Java.OOPConcepts.Inheritance; /*
+ * @Created 13/10/2021/ - 8:33 pm/
+ * @project Hacktoberfest21
+ * @author sanc on
+ */
+
+// derived class
+class MountainBike extends Bicycle {
+
+    // the MountainBike subclass adds one more field
+    public int seatHeight;
+
+    // the MountainBike subclass has one constructor
+    public MountainBike(int gear, int speed,
+                        int startHeight)
+    {
+        // invoking base-class(Bicycle) constructor
+        super(gear, speed);
+        seatHeight = startHeight;
+    }
+
+    // the MountainBike subclass adds one more method
+    public void setHeight(int newValue)
+    {
+        seatHeight = newValue;
+    }
+
+    // overriding toString() method
+    // of Bicycle to print more info
+    @Override public String toString()
+    {
+        return (super.toString() + "\nseat height is "
+                + seatHeight);
+    }
+}
