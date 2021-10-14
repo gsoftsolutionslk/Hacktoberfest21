20ee5966fda69182f69d162ea5ce42b06008c25e
Naresh <chandanbatven@gmail.com>
2021-10-14 23:49:31 +0530

    adding primeChecker.java

---
+++ Java/Hacktoberfest.java
@@ -0,0 +1,5 @@
+public class Hacktoberfest {
+    public static void main(String[] args) {
+        System.out.println("Hello Hacktoberfest");
+    }
+}
---
+++ Java/Hello.java
@@ -0,0 +1,9 @@
+public class Hello {
+    public static void main(String[] args) {
+        String x = "giga";
+        System.out.println(x);
+
+        Thanks t = new Thanks();
+        t.print_thank();
+    }
+}
